6fc9e2a4d11e18cd2d03c02e4d78e5196436cb8f
matuo-0000 <kuniteru.123@gmail.com>
2025-02-28 17:46:57 +0900

    hennkou

---
+++ js/index.js
@@ -0,0 +1,15 @@
+'use strict'
+{
+  const var1 = document.querySelector('.hum_var1');
+  const var2 = document.querySelector('.hum_var2');
+  const var3 = document.querySelector('.hum_var3');
+  const modal = document.querySelector('.modal');
+  const hum = document.querySelector('.hum_frame');
+
+  hum.addEventListener('click', () => {
+    var1.classList.toggle('open');
+    var2.classList.toggle('open');
+    var3.classList.toggle('open');
+    modal.classList.toggle('open');
+  });
+}
